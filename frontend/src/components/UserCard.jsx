a28fb0039aef3eb2bc9466fb43380cf8157b52fc
Parth_dey <130208653+parthdey@users.noreply.github.com>
2025-04-08 12:34:52 +0530

    2-ui design completed

---
+++ frontend/src/components/UserCard.jsx
@@ -0,0 +1,41 @@
+import { Card, CardHeader, Flex, Avatar, Box, Heading, Text, IconButton, CardBody} from '@chakra-ui/react'
+import React from 'react'
+import { BiTrash } from 'react-icons/bi'
+import EditModal from './EditModal'
+
+const UserCard = ({user}) => {
+  return <Card>
+    <CardHeader>
+        <Flex gap={"4"}>
+            <Flex flex={"1"} gap={"4"} alignItems={"center"}>
+                <Avatar src={user.imgUrl} />
+
+                <Box>
+                    <Heading size='sm'>{user.name}</Heading>
+                    <Text>{user.role}</Text>
+                </Box>
+            </Flex>
+
+            <Flex>
+                <EditModal />
+                <IconButton
+							variant='ghost'
+							colorScheme='red'
+							size={"sm"}
+							aria-label='See menu'
+							icon={<BiTrash size={20} />}
+							// onClick={handleDeleteUser}
+						/>
+            </Flex>
+        </Flex>
+    </CardHeader>
+
+    <CardBody>
+        <Text>
+            {user.description}
+        </Text>
+    </CardBody>
+  </Card>
+}
+
+export default UserCard
